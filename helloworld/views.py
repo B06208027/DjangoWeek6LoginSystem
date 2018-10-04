ea297e1e251dc1490f55a359a37fba547e1e6aab
廖威仲 <michael07031224@gmail.com>
2018-10-04 12:35:21 +0800

    week4ver3

--- helloworld/views.py
+++ helloworld/views.py
@@ -10,9 +10,6 @@ def index(request):
 	t1 = TextMessage.objects.create(talker='Michael', message='Hello, Professor!')
 	t2 = TextMessage.objects.create(talker='Pecu', message='Hello, Class!')
 	t3 = TextMessage.objects.create(talker='Domi', message='Hello, Michael!')
-	t1.save()
-	t2.save()
-	t3.save()
 
 	msgs = TextMessage.objects.all()
 
