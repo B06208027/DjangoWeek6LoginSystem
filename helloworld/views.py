14070e5ea39987aabe29c31a6c0b0a3a95ed867a
廖威仲 <michael07031224@gmail.com>
2018-10-04 11:55:45 +0800

    week4ver1

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,7 +3,18 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
+from guestbook.models import TextMessage
 
 def index(request):
-	return render(request, 'guestbookver1.html')
+
+	t1 = TextMessage.objects.create(talker='Michael', message='Hello, Professor!')
+	t2 = TextMessage.objects.create(talker='Pecu', message='Hello, Class!')
+	t3 = TextMessage.objects.create(talker='Domi', message='Hello, Michael!')
+	t1.save()
+	t2.save()
+	t3.save()
+
+	msgs = TextMessage.objects.all()
+
+
+	return render(request, 'guestbookver1.html', locals())
