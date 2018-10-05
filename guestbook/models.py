444471c0d788a1da2cc813d1945f1d7f6dd5cb1a
廖威仲 <michael07031224@gmail.com>
2018-10-05 14:30:40 +0800

    testdb

--- guestbook/models.py
+++ guestbook/models.py
@@ -6,5 +6,6 @@ from django.conf import settings
 class TextMessage(models.Model):
 	talker = models.CharField(max_length=20, blank=False)
 	message = models.CharField(max_length=50, blank=True)
+	talktime = models.CharField(max_length=10, blank=True)
 	def __str__(self):
 		return self.talker + " " + self.message
