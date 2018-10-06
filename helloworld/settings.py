3b5dedea2e2c01a5749543e4843502b153a0c722
廖威仲 <michael07031224@gmail.com>
2018-10-06 18:44:30 +0800

    week5ver4

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x9grjd59tgk-z_pn=0d-7urc%10@l8r@+44&u^w*xr7^u*oonz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
@@ -134,6 +134,5 @@ else:
 
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
