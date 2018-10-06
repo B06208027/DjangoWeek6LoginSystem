816dca34253525a14d3fec799902911a9f40d229
廖威仲 <michael07031224@gmail.com>
2018-10-06 18:27:35 +0800

    t

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x9grjd59tgk-z_pn=0d-7urc%10@l8r@+44&u^w*xr7^u*oonz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
- DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
