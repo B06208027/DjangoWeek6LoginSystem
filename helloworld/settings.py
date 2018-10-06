a0c74ea4cd3b3e31000cf813dfdac85fad7e00c7
廖威仲 <michael07031224@gmail.com>
2018-10-06 16:12:58 +0800

    week5postgres

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'weizhong',
+        'PASSWORD': 'ufen0703',
+        'HOST': '',
+        'PORT': '',
     }
 }
 # import dj_database_url
